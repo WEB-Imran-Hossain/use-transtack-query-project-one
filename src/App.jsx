b30589c200fc03ca8bce1fa12c2a1446d9a02932
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-08-06 01:29:05 +0600

    configer reactquerydev tool

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-
+import ProductList from "./components/ProductList";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ProductList />
     </>
   );
 }
